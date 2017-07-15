b2e126d6048b639d970a117d565f7f388a74b86b
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-15 16:08:59 -0500

    __init__ for flaskr modified to import init_db for tests

--- flaskr/flaskr/__init__.py
+++ flaskr/flaskr/__init__.py
@@ -1,3 +1,4 @@
 # Brings application instance to the top level of the package
 # so that you can do export FLASK_APP = flaskr instead of FLASK_APP = flaskr.flaskr
 from .flaskr import app
+from .flaskr import init_db
