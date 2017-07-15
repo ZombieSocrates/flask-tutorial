2e37b8140bf65b1ac165773e006450ab3758b635
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-15 16:35:13 -0500

    All tests passed; flaskr.db tracked

--- flaskr/tests/test_flaskr.py
+++ flaskr/tests/test_flaskr.py
@@ -30,5 +30,45 @@ class FlaskrTestCase(unittest.TestCase):
 		rv = self.app.get('/')
 		assert b'No entries so far' in rv.data
 
+	def login(self, username, password):
+		return self.app.post('/login', 
+							data = dict(username = username,
+										password = password),
+							follow_redirects = True
+							)
+
+	def logout(self):
+		return self.app.get('/logout', follow_redirects = True)
+
+	def test_login_logout(self):
+		'''Uses the above two methods to see whether you can log in
+		and out with valid credentials and get banhammered with 
+		invalid ones
+		'''
+		rv = self.login('admin','default')
+		assert b'You were logged in' in rv.data
+		rv = self.logout()
+		assert b'You were logged out' in rv.data
+		rv = self.login('adminx','default')
+		assert b'Invalid username' in rv.data
+		rv = self.login('admin','defaultx')
+
+	def test_messages(self):
+		'''Checks that we can add a message, where html is allowed in the
+		text but not in the title
+		'''
+		self.login('admin','default')
+		rv = self.app.post('/add',
+						   data = dict(title='<Hello>',
+						   			   text = '<strong>HTML</strong> allowed here'),
+						   follow_redirects = True
+						   )
+		assert b'No entries so far' not in rv.data
+		assert b'&lt;Hello&gt;' in rv.data
+		assert b'<strong>HTML</strong> allowed here' in rv.data
+
+
+
+
 if __name__ == '__main__':
 	unittest.main()
