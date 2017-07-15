2015582ecdf5838317c2de068a729572fb538d5d
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-15 16:09:17 -0500

    First unit test coded and passed

--- flaskr/tests/test_flaskr.py
+++ flaskr/tests/test_flaskr.py
@@ -6,15 +6,29 @@ import tempfile
 class FlaskrTestCase(unittest.TestCase):
 
 	def setUp(self):
-		self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
+		'''Creates a new test_client and initializes a new database
+		before each test is run.  Also activates testing config flag
+		to true
+		'''
+		self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp() #returns a low-level handle and random file name
 		flaskr.app.testing = True
 		self.app = flaskr.app.test_client()
 		with flaskr.app.app_context():
 			flaskr.init_db()
 
 	def tearDown(self):
+		'''clean up,clean up, everybody everywhere
+		'''
 		os.close(self.db_fd)
 		os.unlink(flaskr.app.config['DATABASE'])
+
+	def test_empty_db(self):
+		'''Checks that application has "No entries so far"
+		at its root before any entries are added. The 
+		app.get method sends a get request and we check the
+		output'''
+		rv = self.app.get('/')
+		assert b'No entries so far' in rv.data
 
 if __name__ == '__main__':
 	unittest.main()
