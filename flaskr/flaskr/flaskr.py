c72b235716d19c3b0b445091602bd0f4ed1729de
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-15 13:20:44 -0500

    First view function: show_entries

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -67,3 +67,15 @@ def initdb_command():
 	'''Initializes the database'''
 	init_db()
 	print('Initialized  the database')
+
+'''This view function passes the entries in the database
+to the show_entries template and renders the template
+'''
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries = entries)
+
+# @app.route('/add', methods = ['POST'])
