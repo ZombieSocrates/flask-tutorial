41ff75cc5fe1ec9879181401b73e607a8caaa7e5
Ross Weijer <ross.weijer@datascopeanalytics.com>
2017-07-15 12:42:36 -0500

    Open and close connection functions defined

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -3,6 +3,37 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template, flash
 
+# Flask provides two contexts: the application context and  the request context
+# The request variable is associated with the current request, while
+# g is an all purpose variable defining the current application context
+
+
+def connect_db():
+	'''connects to app.config['DATABASE']'''
+	rv = sqlite3.connect(app.config['DATABASE'])
+	rv.row_factory = sqlite3.Row # Treats rows as dictionaries instead of tuples
+	return rv
+
+def get_db():
+	''' Opens a new database connection if there isn't one in the
+	current application context
+	'''
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db = connect_db()
+	return g.sqlite_db
+
+# Functions with this decorator will execute everytime the appcontext
+# tears down. This context is created before a request is made and torn
+# down when the request finishes. The teardown will have an error parameter
+# of None (everything is fine) or an exception happened
+@app.teardown_appcontext
+def close_db(error):
+	'''Closes the database again at the end of a request
+	'''
+	if hasattr(g, 'sqlite_db'):
+		g.sqlite_db.close()
+
+
 
 app = Flask(__name__) # create the application instance
 app.config.from_object(__name__) # get config settings from this file
@@ -20,8 +51,3 @@ app.config.update(new_config)
 # will ignore this if FLASKR_SETTINGS is not present
 app.config.from_envvar('FLASKR_SETTINGS', silent = True)
 
-def connect_db():
-	'''connects to app.config['DATABASE']'''
-	rv = sqlite3.connect(app.config['DATABASE'])
-	rv.row_factory = sqlite3.Row # Treats rows as dictionaries instead of tuples
-	return rv
